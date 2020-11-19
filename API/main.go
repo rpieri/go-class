1af436f1b5c664738b77518d7f8676df2b85633e
Raphael De Pieri Poi Fulgencio <raphaeldppf@hotmail.com>
2020-11-18 21:02:06 -0300

    Teste

--- API/main.go
+++ API/main.go
@@ -12,6 +12,7 @@ func main() {
 	config.Carregar()
 	r := router.Gerar()
 
+
 	fmt.Printf("Escutando na porta: %d", config.Porta)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
